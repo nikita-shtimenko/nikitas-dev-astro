832c7c7e8c66ef6832b6de1cdfb50e8ab9ddb350
nikita-shtimenko <nikitashtimenko.dev@gmail.com>
2023-12-01 21:44:30 +0200

    vercelStatic -> vercelServerless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-// import vercel from "@astrojs/vercel/serverless"; DOEST WORK
-import vercelStatic from "@astrojs/vercel/static"; // TESTING RN
+import vercel from "@astrojs/vercel/serverless";
+// import vercelStatic from "@astrojs/vercel/static"; // MOBILE NAVBAR WORKS, PATHNAME DOES NOT WORK
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "static",
-  adapter: vercelStatic(),
+  output: "server",
+  adapter: vercel(),
 });
