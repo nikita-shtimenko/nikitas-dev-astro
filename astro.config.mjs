1577e78edfeb8a9dcdbebaaef5cfda68644def42
nikita-shtimenko <nikitashtimenko.dev@gmail.com>
2023-12-01 21:39:53 +0200

    move to vercelStatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercelEdge from "@astrojs/vercel/edge"; // TESTING RN
 // import vercel from "@astrojs/vercel/serverless"; DOEST WORK
-// import vercelStatic from "@astrojs/vercel/static"; ?
+import vercelStatic from "@astrojs/vercel/static"; // TESTING RN
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercelEdge(),
+  adapter: vercelStatic(),
 });
